80be6583a44c015df0ef5a73d0af43d4b31b4134
Anaël iMac <anael@barodine.fr>
2021-08-07 03:34:47 +0200

    fix: removed Main class

--- src/main/java/fr/bde_tribu_terre/discord/mcbot/Main.java
+++
@@ -1,9 +0,0 @@
-package fr.bde_tribu_terre.discord.mcbot;
-
-import fr.barodine.anael.discord.launcher.Launcher;
-
-public class Main {
-    public static void main(String[] args) {
-        Launcher.launch();
-    }
-}
